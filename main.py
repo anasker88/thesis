8d3d5b3bfa61fe0c44d84f2ea1f1417a2b9f32ec
anasker88 <decrypt.cabins0i@icloud.com>
2024-12-23 20:07:09 +0900

    visualization

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 import os
 
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import torch
 from run_and_eval import *
 from sae_lens import SAE, HookedSAETransformer
 from tqdm import tqdm
+
+
+def show_activation(st_act, anti_st_act, layer, feature, language, layers):
+    lang = ["en", "jp"]
+    st_act = st_act[language, layer, :, feature]
+    anti_st_act = anti_st_act[language, layer, :, feature]
+    n_bins = 50
+    max_val = max(st_act.max(), anti_st_act.max())
+    min_val = min(st_act.min(), anti_st_act.min())
+    bins = np.linspace(min_val, max_val, n_bins)
+    plt.hist(st_act, bins=bins, alpha=0.5, label="stereotypical")
+    plt.hist(anti_st_act, bins=bins, alpha=0.5, label="anti-stereotypical")
+    plt.legend()
+    plt.title(f"Layer {layer}, Feature {feature}, {lang[language]}")
+    plt.savefig(
+        f"{output_dir}/{lang[language]}/layer{layers[layer]}_feature{feature}.png"
+    )
+    plt.close()
+
 
 llm_names = ["gpt2-small", "meta-llama/Llama-3.1-8B", "gemma-2-9b"]
 sae_names = ["gpt2-small-res-jb", "llama_scope_lxm_8x", "google/gemma-scope-9b-pt-mlp"]
@@ -15,11 +36,14 @@ sae_ids = [
 
 target_llm = 0
 # 0:gpt-2-small, 1:llama-3.1-8B, 2:gemma-2-9b
+k = 10
 llm_name = llm_names[target_llm]
 sae_release = sae_names[target_llm]
 sae_id = sae_ids[target_llm]
 output_dir = f"out/{sae_release}"
 os.makedirs(output_dir, exist_ok=True)
+os.makedirs(f"{output_dir}/jp", exist_ok=True)
+os.makedirs(f"{output_dir}/en", exist_ok=True)
 
 # initialize
 torch.set_grad_enabled(False)
@@ -40,7 +64,11 @@ jp_data = pd.read_csv("data/jp_data.csv")
 
 layer_num = model.cfg.n_layers
 print(f"Layer num: {layer_num}")
-bar = tqdm(range(0, layer_num, 4))
+layers = range(0, layer_num, 10)
+bar = tqdm(layers)
+st_act_list = [[], []]
+anti_st_act_list = [[], []]
+diff_sum_list = [[], []]
 with open(f"{output_dir}/output.txt", "w") as f:
     for layer in bar:
         bar.set_description(f"Loading SAE for layer {layer}")
@@ -64,6 +92,8 @@ with open(f"{output_dir}/output.txt", "w") as f:
             # get activations
             st_act = get_activations(model, sae, st_prompt, target)
             anti_st_act = get_activations(model, sae, anti_st_prompt, target)
+            st_act_list[i].append(st_act)
+            anti_st_act_list[i].append(anti_st_act)
             # remove same pairs
             st_act, anti_st_act = remove_same_pairs(st_act, anti_st_act)
             f.write(f"Language: {'en' if i == 0 else 'jp'}\n")
@@ -72,9 +102,10 @@ with open(f"{output_dir}/output.txt", "w") as f:
             f.write(f"Important features: {len(important_features)}\n")
             small_st_act = st_act[:, important_features]
             small_anti_st_act = anti_st_act[:, important_features]
-            k = min(10, len(important_features))
+            k = min(k, len(important_features))
             # evaluate by diff
-            diff_sum, vals, inds = evaluate_by_diff(small_st_act, small_anti_st_act, k)
+            diff_sum, vals, inds = evaluate_by_diff(st_act, anti_st_act, k)
+            diff_sum_list[i].append(diff_sum)
             # 2-class classification by random forest
             accuracy_rf, feature_importance_rf = run_random_forest(
                 small_st_act, small_anti_st_act
@@ -86,7 +117,7 @@ with open(f"{output_dir}/output.txt", "w") as f:
             avg_diff = (st_act - anti_st_act).abs().sum(dim=0).mean()
             scores = activation_score(st_act, anti_st_act)
             f.write(f"Top k features: {vals}\n")
-            f.write(f"Top k feature indices: {important_features[inds]}\n")
+            f.write(f"Top k feature indices: {inds}\n")
             f.write(f"Average of the absolute difference: {avg_diff}\n")
             # f.write(f"Accuracy by random forest: {accuracy_rf}\n")
             # vals, inds = torch.topk(feature_importance_rf, k)
@@ -94,11 +125,35 @@ with open(f"{output_dir}/output.txt", "w") as f:
             # f.write(
             #     f"Top k feature indices by random forest: {important_features[inds]}\n"
             # )
-            f.write(f"Accuracy by SVM: {accuracy_svm}\n")
-            vals, inds = torch.topk(normal_vector_svm.abs(), k)
-            f.write(f"Top k features activations by SVM: {diff_sum[inds]}\n")
-            f.write(f"Top k feature indices by SVM: {important_features[inds]}\n")
-            vals, inds = torch.topk(scores, k)
-            f.write(f"Top k features activations by score: {vals}\n")
-            f.write(f"Top k feature indices by score: {inds}\n")
-            f.write("\n")
+            # f.write(f"Accuracy by SVM: {accuracy_svm}\n")
+            # vals, inds = torch.topk(normal_vector_svm.abs(), k)
+            # f.write(f"Top k features activations by SVM: {diff_sum[inds]}\n")
+            # f.write(f"Top k feature indices by SVM: {important_features[inds]}\n")
+            # vals, inds = torch.topk(scores, k)
+            # f.write(f"Top k features activations by score: {vals}\n")
+            # f.write(f"Top k feature indices by score: {inds}\n")
+            # f.write("\n")
+    layer_num = len(st_act_list[0])
+    sentences = len(st_act_list[0][0])
+    features = len(st_act_list[0][0][0])
+    # テンソルに変換
+    st_act_tensor = torch.zeros(2, layer_num, sentences, features)
+    anti_st_act_tensor = torch.zeros(2, layer_num, sentences, features)
+    for i in range(layer_num):
+        st_act_tensor[0, i] = st_act_list[0][i]
+        st_act_tensor[1, i] = st_act_list[1][i]
+        anti_st_act_tensor[0, i] = anti_st_act_list[0][i]
+        anti_st_act_tensor[1, i] = anti_st_act_list[1][i]
+    for i in range(2):
+        f.write(f"Language: {'en' if i == 0 else 'jp'}\n")
+        vals, inds = torch.topk(torch.cat(diff_sum_list[i], dim=0), k)
+        f.write(f"Top k features activations by diff: {vals}\n")
+        for j in range(k):
+            ind = inds[j]
+            top_k = (ind // features, ind % features)
+            f.write(
+                f"Top k feature indices by diff: Layer {layers[top_k[0]]}, Feature {top_k[1]}\n"
+            )
+            show_activation(
+                st_act_tensor, anti_st_act_tensor, top_k[0], top_k[1], i, layers
+            )
