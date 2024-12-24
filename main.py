5c4bb4ddd66f9db4d38696587dad586625bdcdf6
anasker88 <decrypt.cabins0i@icloud.com>
2024-12-24 18:16:18 +0900

    memory save

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ from sae_lens import SAE, HookedSAETransformer
 from tqdm import tqdm
 
 
-def show_activation(st_act, anti_st_act, layer, feature, language, layers):
+def show_activation(st_act, anti_st_act, layer, feature, language):
     lang = ["en", "jp"]
-    st_act = st_act[language, layer, :, feature]
-    anti_st_act = anti_st_act[language, layer, :, feature]
     n_bins = 50
+    st_act = st_act.cpu().numpy()
+    anti_st_act = anti_st_act.cpu().numpy()
     max_val = max(st_act.max(), anti_st_act.max())
     min_val = min(st_act.min(), anti_st_act.min())
     bins = np.linspace(min_val, max_val, n_bins)
@@ -21,9 +21,7 @@ def show_activation(st_act, anti_st_act, layer, feature, language, layers):
     plt.hist(anti_st_act, bins=bins, alpha=0.5, label="anti-stereotypical")
     plt.legend()
     plt.title(f"Layer {layer}, Feature {feature}, {lang[language]}")
-    plt.savefig(
-        f"{output_dir}/{lang[language]}/layer{layers[layer]}_feature{feature}.png"
-    )
+    plt.savefig(f"{output_dir}/{lang[language]}/layer{layer}_feature{feature}.png")
     plt.close()
 
 
@@ -69,6 +67,7 @@ bar = tqdm(layers)
 st_act_list = [[], []]
 anti_st_act_list = [[], []]
 diff_sum_list = [[], []]
+inds_list = [[], []]
 with open(f"{output_dir}/output.txt", "w") as f:
     for layer in bar:
         bar.set_description(f"Loading SAE for layer {layer}")
@@ -92,26 +91,27 @@ with open(f"{output_dir}/output.txt", "w") as f:
             # get activations
             st_act = get_activations(model, sae, st_prompt, target)
             anti_st_act = get_activations(model, sae, anti_st_prompt, target)
-            st_act_list[i].append(st_act.cpu())
-            anti_st_act_list[i].append(anti_st_act.cpu())
-            # remove same pairs
-            st_act, anti_st_act = remove_same_pairs(st_act, anti_st_act)
+            # # remove same pairs
+            # st_act, anti_st_act = remove_same_pairs(st_act, anti_st_act)
             f.write(f"Language: {'en' if i == 0 else 'jp'}\n")
-            f.write(f"samples: {len(st_act)}\n")
-            important_features = filter_out_features(st_act, anti_st_act)
-            f.write(f"Important features: {len(important_features)}\n")
-            small_st_act = st_act[:, important_features]
-            small_anti_st_act = anti_st_act[:, important_features]
-            k = min(k, len(important_features))
+            # f.write(f"samples: {len(st_act)}\n")
+            # important_features = filter_out_features(st_act, anti_st_act)
+            # f.write(f"Important features: {len(important_features)}\n")
+            # small_st_act = st_act[:, important_features]
+            # small_anti_st_act = anti_st_act[:, important_features]
+            # k = min(k, len(important_features))
             # evaluate by diff
             diff_sum, vals, inds = evaluate_by_diff(st_act, anti_st_act, k)
-            diff_sum_list[i].append(diff_sum.cpu())
-            # 2-class classification by random forest
-            accuracy_rf, feature_importance_rf = run_random_forest(
-                small_st_act, small_anti_st_act
-            )
-            # 2-class classification by SVM
-            accuracy_svm, normal_vector_svm = run_svm(small_st_act, small_anti_st_act)
+            diff_sum_list[i].append(diff_sum[inds])
+            st_act_list[i].append(st_act[:, inds])
+            anti_st_act_list[i].append(anti_st_act[:, inds])
+            inds_list[i].append(inds)
+            # # 2-class classification by random forest
+            # accuracy_rf, feature_importance_rf = run_random_forest(
+            #     small_st_act, small_anti_st_act
+            # )
+            # # 2-class classification by SVM
+            # accuracy_svm, normal_vector_svm = run_svm(small_st_act, small_anti_st_act)
             # save results
             # f.write(f"Diff sum: {diff_sum}\n")
             avg_diff = (st_act - anti_st_act).abs().sum(dim=0).mean()
@@ -133,31 +133,24 @@ with open(f"{output_dir}/output.txt", "w") as f:
             # f.write(f"Top k features activations by score: {vals}\n")
             # f.write(f"Top k feature indices by score: {inds}\n")
             # f.write("\n")
-            del st_act, anti_st_act, small_st_act, small_anti_st_act
+            del st_act, anti_st_act
             torch.cuda.empty_cache()
         del sae
         torch.cuda.empty_cache()
-    layer_num = len(st_act_list[0])
-    sentences = len(st_act_list[0][0])
-    features = len(st_act_list[0][0][0])
-    # テンソルに変換
-    st_act_tensor = torch.zeros(2, layer_num, sentences, features)
-    anti_st_act_tensor = torch.zeros(2, layer_num, sentences, features)
-    for i in range(layer_num):
-        st_act_tensor[0, i] = st_act_list[0][i]
-        st_act_tensor[1, i] = st_act_list[1][i]
-        anti_st_act_tensor[0, i] = anti_st_act_list[0][i]
-        anti_st_act_tensor[1, i] = anti_st_act_list[1][i]
     for i in range(2):
         f.write(f"Language: {'en' if i == 0 else 'jp'}\n")
         vals, inds = torch.topk(torch.cat(diff_sum_list[i], dim=0), k)
         f.write(f"Top k features activations by diff: {vals}\n")
         for j in range(k):
             ind = inds[j]
-            top_k = (ind // features, ind % features)
+            top_k = (ind // k, ind % k)
             f.write(
-                f"Top k feature indices by diff: Layer {layers[top_k[0]]}, Feature {top_k[1]}\n"
+                f"Top k feature indices by diff: Layer {layers[top_k[0]]}, Feature {inds_list[i][top_k[0]][top_k[1]]}\n"
             )
             show_activation(
-                st_act_tensor, anti_st_act_tensor, top_k[0], top_k[1], i, layers
+                st_act_list[i][top_k[0]][:, top_k[1]],
+                anti_st_act_list[i][top_k[0]][:, top_k[1]],
+                layers[top_k[0]],
+                inds_list[i][top_k[0]][top_k[1]],
+                i,
             )
